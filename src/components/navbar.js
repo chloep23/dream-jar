341dff997a2f8df1f7e8fdcf46d534602bef46b1
chloep23 <chloesp23@gmail.com>
2024-08-01 14:46:45 -0400

    Worked on HomePage

---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+"use client";
+
+import Link from 'next/link';
+
+export default function NavBar() {
+  return (
+      <nav className="bg-purple p-2 flex justify-between items-center h-20 w-full">
+        <ul className="flex justify-left flex-wrap text-white items-left mt-4 md:mt-0">DreamJar</ul>
+        <img src="/assets/logo.png" alt="Company Logo" className="h-10 w-auto"/>
+      </nav>
+
+      
+  )
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,25 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors:{
+      'white': '#FFFFFF',
+      'beigeWhite': '#F7F7F7', 
+      'black': '#000000',
+      'gray': '#797575',
+      'purple': '#7856FF',
+      'light-blue':'#DAF5F2',
+      'light-yellow':'#FCF9E2',
+      'light-orange': '#F8E2D9',
+      'light-pink': '#FFE4F9',
+      'blue':'#9CE7E2',
+      'yellow': '#E7D38B',
+      'orange': '#F8A991', 
+      'pink': '#F6A6E5',
+    },
+
+    fontFamily:{
+      'lato-bold': ['lato-bold'],
+    },
   },
   plugins: [],
 };
