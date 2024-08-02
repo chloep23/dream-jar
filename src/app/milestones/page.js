b2177f58fdee47a589dc46c7d8f829c32a54118d
chloep23 <chloesp23@gmail.com>
2024-08-02 10:22:40 -0400

    fixed trademark

--- src/app/milestones/page.js
+++ src/app/milestones/page.js
@@ -9,6 +9,7 @@ export default function Milestonees() {
                 <h1 className="text-black text-center text-3xl md:text-4xl font-bold mx-auto md:mb-4 ">Coming Soon!</h1>
                 <h2 className="text-gray text-center text-xl font-bold mb-8 tracking-normal">Keep on Dreaming</h2>
             </div>
+            
         </div>
     )
 };
