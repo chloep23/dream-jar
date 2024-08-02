bfdb50f7b216f594ddb201ea60d81b942220468f
chloep23 <chloesp23@gmail.com>
2024-08-02 10:18:46 -0400

    Final Changes

--- src/app/transactions/page.js
+++ src/app/transactions/page.js
@@ -73,8 +73,8 @@ export default function Dreamers() {
             </div>
             {/*---------FOOTNOTE ----------*/}
             <div className="flex flex-col items-center mx-auto left-0 right-0 mt-5 p-5">
-            <h3 className = "text-gray text-center text-lg font-bold">All Rights Reserved @CapitalOneBHTS</h3>
-        </div>
+                <h3 className = "text-gray text-center text-lg font-bold">All Rights Reserved @CapitalOneBHTS</h3>
+             </div>
         </div>
     </div>
     )
